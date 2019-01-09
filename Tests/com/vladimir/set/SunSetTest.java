f50b580144e8a48ccd736f0ce22c690edfff5d5a
Vladimir <vova.kvz3@gmail.com>
2019-01-09 14:27:26 +0200

    ""

--- Tests/com/vladimir/set/SunSetTest.java
+++ Tests/com/vladimir/set/SunSetTest.java
@@ -3,7 +3,12 @@ package com.vladimir.set;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
